9c13b903519ab6a7349ace8dcdbebc98321f0387
carloskore <carlosmiquel88@gmail.com>
2026-02-21 09:16:22 +0100

    Fins a J25

--- html/admin/saveKnockOut.php
+++ html/admin/saveKnockOut.php
@@ -7,10 +7,12 @@ $comp = $_GET["comp"];
 $season = $_GET["season"];
 
 // Convert JSON string to Object
-//$someObject = json_decode($data);
+// $someObject = json_decode($data);
 $log = "";
+$sql = "";
 
-echo $someObject;
+// echo $season;
+// echo $someObject;
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -19,13 +21,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$id = $value->id+1;
-$quer = "UPDATE KNOCKOUT_TABLE SET JSON_STRING ='" . $data . "', UPDATE_DTM = NOW() WHERE TROPHY = '" . $comp . "' AND SEASON = " . $season . ";"  ;
+// $id = $value->id+1;
+$quer = "UPDATE KNOCKOUT_TABLE SET JSON_STRING ='" . $data . "', UPDATE_DTM = NOW() WHERE TROPHY = '" . $comp . "' AND SEASON = " . $season . ";";
 $sql .= $quer;
 $log .= date("d/m/Y") . " - " . date("h:i:sa") . " - Updating knocckout for " . $comp . " and season " . $season . PHP_EOL;
 
 
-//echo $sql;
+echo $sql;
 
 $return = "";
 
